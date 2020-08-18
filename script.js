065ad05b477e22c3d0fcacd3408a579ef3007d26
Gretchen Krammes <gkkrammes@gmail.com>
2020-08-18 18:10:24 -0400

    Edits and stylinig

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var day1 = moment().calendar();  
-var day2 = moment().add(1, 'days').calendar();
-var day3 = moment().add(2, 'days').calendar();
-var day4 = moment().add(3, 'days').calendar();
-var day5 = moment().add(4, 'days').calendar();
+
+var currentDay = moment().format('M/DD/YYYY');
+var day1 = moment().add(1, 'days').format('M/DD/YYYY');
+var day2 = moment().add(2, 'days').format('M/DD/YYYY');
+var day3 = moment().add(3, 'days').format('M/DD/YYYY');
+var day4 = moment().add(4, 'days').format('M/DD/YYYY');
+var day5 = moment().add(5, 'days').format('M/DD/YYYY');
 
 
 $(document).ready(function() {
@@ -20,7 +22,7 @@ $(document).ready(function() {
     getForecast(location);
 });
 
-function getForecast(location) {
+    function getForecast(location) {
  
     $("#dailyForecast").empty();
     $("#fiveDayForecast").empty();
@@ -30,7 +32,7 @@ function getForecast(location) {
     $("#day4").empty();
     $("#day5").empty();
 
-    var daily ="https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=2578c302717c0f0c5906c71e6983eae6";
+    var daily ="https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=2578c302717c0f0c5906c71e6983eae6";
         console.log("Daily", daily);  
 
 
@@ -53,7 +55,7 @@ function getForecast(location) {
         + "</div>"
     ); 
 
-    var fiveDay = "https://api.openweathermap.org/data/2.5/onecall?" + "lat=" + lat + "&lon=" + lon + "&appid=2578c302717c0f0c5906c71e6983eae6";  
+    var fiveDay = "https://api.openweathermap.org/data/2.5/onecall?" + "lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=2578c302717c0f0c5906c71e6983eae6";  
         console.log("fiveDay", fiveDay);
 
     $.ajax({
@@ -68,7 +70,80 @@ function getForecast(location) {
     var iconLink4 = "http://openweathermap.org/img/w/" + response.daily[3].weather[0].icon + ".png";
     var iconLink5 = "http://openweathermap.org/img/w/" + response.daily[4].weather[0].icon + ".png";
     
-    })
-})
-}
-});
+    $("#fiveDayForecast").append(
+        "<div class='col-md-12'>"
+    );
+
+    $("#day1").append(
+        "<div class='fiveDayForecastCard card col s12 m6'>"  +  "<div class='card-body'>"
+        +  "<div class='card-header'>" + day1 +"</div>"
+        +  "<div class='card-text'>" + "<img src='" + iconLink1 + "'>" +"</div>"
+        +  "<div class='card-text'>" + "Temperature:" + response.daily[0].temp.day + " °F" + "</div>"
+        +  "<div class='card-text'>" + "Humidity: " + response.daily[0].humidity + "%" + "</div>" 
+        + "</div>" 
+       ); 
+
+    $("#day2").append(
+        "<div class='fiveDayForecastCard card col s12 m6'>"  +  "<div class='card-body'>"
+        +  "<div class='card-header'>" + day2 +"</div>"
+        +  "<div class='card-text'>" + "<img src='" + iconLink2 + "'>" +"</div>"
+        +  "<div class='card-text'>" + "Temperature:" + response.daily[1].temp.day + " °F" + "</div>"
+        +  "<div class='card-text'>" + "Humidity: " + response.daily[1].humidity + "%" + "</div>" 
+        + "</div>" 
+       ); 
+
+    $("#day3").append(
+        "<div class='fiveDayForecastCard card col s12 m6'>"  +  "<div class='card-body'>"  
+        +  "<div class='card-header'>" + day3 +"</div>"
+        +  "<div class='card-text'>" + "<img src='" + iconLink3 + "'>" +"</div>"
+        +  "<div class='card-text'>" + "Temperature:" + response.daily[2].temp.day + " °F" + "</div>"
+        +  "<div class='card-text'>" + "Humidity: " + response.daily[2].humidity + "%" + "</div>" 
+        + "</div>" 
+       ); 
+
+    $("#day4").append(
+        "<div class='fiveDayForecastCard card col s12 m6'>"  +  "<div class='card-body'>"
+        +  "<div class='card-header'>" + day4 +"</div>"
+        +  "<div class='card-text'>" + "<img src='" + iconLink4 + "'>" +"</div>"
+        +  "<div class='card-text'>" + "Temperature:" + response.daily[3].temp.day + " °F" + "</div>"
+        +  "<div class='card-text'>" + "Humidity: " + response.daily[3].humidity + "%" + "</div>" 
+        + "</div>" 
+       ); 
+
+    $("#day5").append(
+        "<div class='fiveDayForecastCard card col s12 m6'>"  +  "<div class='card-body'>"
+        +  "<div class='card-header'>" + day5 +"</div>"
+        +  "<div class='card-text'>" + "<img src='" + iconLink5 + "'>" +"</div>"
+        +  "<div class='card-text'>" + "Temperature:" + response.daily[4].temp.day + " °F" + "</div>"
+        +  "<div class='card-text'>" + "Humidity: " + response.daily[4].humidity + "%" + "</div>" 
+        + "</div>" 
+       ); 
+
+        })
+      })
+    }
+    function getLocations() {
+        $("#locationList").empty();  
+        var locationListArray = JSON.parse(localStorage.getItem("locationList")) || [];
+        var arrayLength = locationListArray.length; 
+      
+        for (var i = 0; i < arrayLength; i++) { 
+          var locationFromArray = locationListArray[i]; 
+      
+          $("#locationList").append (
+            
+            "<div class='list-group'>" + "<button class='list-group-item'>" + locationFromArray  + "</button>")
+          }
+        }
+
+        getLocations();
+
+
+    $("#locationList").on("click", ".list-group-item", function(event) {
+        event.preventDefault();
+        var location = ($(this).text());
+        getForecast(location); 
+    }) 
+              
+}); 
+
